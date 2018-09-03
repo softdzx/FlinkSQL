7cc02f44e9251dfb6db45e113426478296787a98
ambition119 <1269223860>
2018-09-03 11:17:52 +0800

    source provide modify

--- blink-client/src/main/java/pingle/wang/client/table/FlinkTableCatalogProvider.java
+++ blink-client/src/main/java/pingle/wang/client/table/FlinkTableCatalogProvider.java
@@ -3,7 +3,6 @@ package pingle.wang.client.table;
 import org.apache.flink.table.api.TableSchema;
 import org.apache.flink.table.sources.TableSource;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -13,8 +12,6 @@ public interface FlinkTableCatalogProvider {
     String getType();
 
     TableSource getInputTableSource(Map<String,String> props, TableSchema schema);
-
-    FlinkTableCatalog getInputCatalog(Map<String,String> props, TableSchema schema);
 
     FlinkTableCatalog getOutputCatalog(Map<String,String> props, TableSchema schema);
 }
